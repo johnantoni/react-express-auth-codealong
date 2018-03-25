597e18c39fa2b75c9aca9d1df0669d453c160fd8
John Griffiths <john@uxgent.com>
2018-03-25 15:59:36 -0400

    working.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,29 +14,51 @@ class Dashboard extends React.Component {
     });
   };
 
-  componentDidMount() {
+  getTodos = () => {
     // 1. When the dashboard loads, get the user's token
-    const token = getToken()
+    const token = getToken();
     // 2. Send a GET request to /todo and pass the token to grab a list of ONLY this user's todos
-    axios.get('/todo', {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-    .then(res => {
-        if (res.status === 200) {
-            const todos = res.data.payload;
-            // 3. If we get a successful response, store the todos in   state.
-            this.setState({ todos });
+    axios
+        .get("/todo", {
+        headers: {
+            Authorization: `Bearer ${token}`
         }
-    })
+        })
+        .then(res => {
+            if (res.status === 200) {
+                const todos = res.data.payload;
+                // 3. If we get a successful response, store the todos in state.
+                this.setState({ todos });
+            }
+        });
+  };
+
+  componentDidMount() {
+    this.getTodos();
   }
+
+  // componentDidUpdate() {
+  //   this.getTodos();
+  // }
 
   handleSubmit = e => {
     e.preventDefault();
     const { todo } = this.state;
 
+    const token = getToken()
     // 1. Get the user's token
+
+    axios
+      .post("/todo",
+        {
+          description: todo
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        .then(this.getTodos);
     // 2. Send a POST to /todo with
     //  a - the body containing the TODO we wish to post
     //  b - the Authorization Header Bearer <token>
